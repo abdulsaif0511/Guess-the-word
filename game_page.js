976d9d9f8b075311d1c5909ff445c591d4c6f4f7
abdulsaif0511 <82303862+abdulsaif0511@users.noreply.github.com>
2022-01-18 13:46:45 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -6,6 +6,10 @@ player1_score=0;
 
 player2_score=0;
 
+question_turn = "player1";
+
+answer_turn   = "player2";
+
 document.getElementById("player1_name").innerHTML=player1_name+" : ";
 
 document.getElementById("player2_name").innerHTML=player2_name+" : ";
@@ -64,3 +68,92 @@ document.getElementById("output").innerHTML = row;
 document.getElementById("word").value =  "";
 
 }
+
+function check(){
+
+get_answer = document.getElementById("input_check_box").value;
+
+answer = get_answer.toLowerCase();
+
+console.log("answer in lower case" + answer);
+
+if(answer == word){
+
+if(answer_turn == "player1"){
+
+    player1_score = player1_score + 1;
+
+    document.getElementById("player1_score").innerHTML = player1_score;
+
+}
+
+else{
+
+    player2_score = player2_score + 1;
+
+    document.getElementById("player2_score").innerHTML = player2_score;
+
+}
+
+}
+ 
+if(question_turn == "player1"){
+
+    question_turn = "player2";
+
+    document.getElementById("player_question").innerHTML = "Question Turn -" + player2_name;
+
+}
+
+else{
+
+    
+    question_turn = "player1";
+
+    document.getElementById("player_question").innerHTML = "Question Turn -" + player1_name;
+
+}
+
+if(answer_turn == "player1"){
+
+    answer_turn = "player2";
+
+    document.getElementById("player_answer").innerHTML = "Answer Turn -" + player2_name;
+
+}
+
+else{
+
+    answer_turn = "player1";
+
+    document.getElementById("player_answer").innerHTML = "Answer Turn -" + player1_name;
+
+}
+
+document.getElementById("output").innerHTML = "";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
